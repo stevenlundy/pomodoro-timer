8471a324359dbe927e96ca8efa546ddc5fedac06
Steven Lundy <steven.lundy@gmail.com>
2016-01-12 10:25:30 -0500

    Add method to advance timer

--- app.js
+++ app.js
@@ -3,16 +3,32 @@ var PomodoroClock = React.createClass({
     return {
       sessionLength: this.props.initialSession,
       breakLength: this.props.initialBreak,
-      running: false,
+      running: true,
       mode: 'session',
       timeRemaining: 0
     };
   },
+
+  advanceTimer: function() {
+    if(this.state.running){
+      if(this.state.timeRemaining > 0){
+        this.setState({
+          timeRemaining: this.state.timeRemaining - 1
+        });
+      } else {
+        this.setState({
+          running: false
+        });
+      }
+    }
+  },
+
   setTimer: function(numMinutes) {
     this.setState({
       timeRemaining: 60 * numMinutes
     });
   },
+
   formatTime: function(seconds) {
     var hours = Math.floor(seconds / 3600);
     var minutes = Math.floor((seconds % 3600) / 60);
@@ -23,6 +39,7 @@ var PomodoroClock = React.createClass({
       return minutes + ':' + this.zeroPad(seconds, 2);
     }
   },
+
   zeroPad: function(num, digits) {
     num = String(num);
     while(num.length < digits){
@@ -30,9 +47,12 @@ var PomodoroClock = React.createClass({
     }
     return num;
   },
+
   componentDidMount: function() {
     this.setTimer(this.state.sessionLength);
+    setInterval(this.advanceTimer, 1000);
   },
+
   render: function() {
     return (
       <div>
