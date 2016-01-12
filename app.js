4ef78b541a7f9abcfd2540d15d663d51719b1579
Steven Lundy <steven.lundy@gmail.com>
2016-01-12 09:54:49 -0500

    Set up basic React app

---
+++ app.js
@@ -0,0 +1,14 @@
+var PomodoroClock = React.createClass({
+  render: function() {
+    return (
+      <div>
+        TEST
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <PomodoroClock />,
+  document.getElementById('content')
+);
